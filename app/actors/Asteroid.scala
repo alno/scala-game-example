83acef19f64d8b953b2be675517c4e9a324f6563
Alexey Noskov <alexey.noskov@gmail.com>
2012-06-03 19:53:21 +0400

    Moving asteroids?..

--- app/actors/Asteroid.scala
+++ app/actors/Asteroid.scala
@@ -12,17 +12,24 @@ import akka.util.Timeout
 import akka.pattern.ask
 
 import play.api.Play.current
+import scala.math._
+
+case class MoveByCommand(dx: Double, dy: Double, drot: Double)
 
 class Asteroid(world: ActorRef, var pos: Pos) extends Actor {
 
   def receive = {
-    case MoveCommand(newPos) =>
-      pos = newPos
+    case MoveByCommand(dx, dy, drot) =>
+      pos = pos.copy(x = pos.x + dx, y = pos.y + dy, rot = pos.rot + drot)
       world ! ObjectMoved(self, pos)
   }
 
   override def preStart = {
     world ! ObjectCreated(self, ObjectState("Asteroid", null, pos, 60))
+
+    /*context.system.scheduler.schedule(0.1 second, 0.1 second) {
+      self ! MoveByCommand(random * 4 - 2, random * 4 - 2, random * 2 - 1)
+    }*/
   }
 
   override def postStop = {
